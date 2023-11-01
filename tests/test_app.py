a01a26b9445108010148b85a3d6c61d0263fde76
gauravk <gauravk@apple.com>
2023-11-01 22:55:42 +0530

    added

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import sys
 import os
 # Convert the relative path to an absolute path
-relative_path = '../src'
+relative_path = './src'
 absolute_path = os.path.abspath(relative_path)
 
 # Append the absolute path to sys.path
