6a01fb880139264375170ab064bc832a37ea57d2
gauravk <gauravk@apple.com>
2023-11-01 22:38:49 +0530

    dir change s

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,12 @@
 import sys
-sys.path.append('/Users/kumargaurav/Desktop/python-ci/src')
-#from src import app 
-from app import index
+import os
+# Convert the relative path to an absolute path
+relative_path = './src'
+absolute_path = os.path.abspath(relative_path)
+
+# Append the absolute path to sys.path
+sys.path.append(absolute_path)
+#from app import index
 def test_index():
-    assert index() == "Hello, world!"
+    assert absolute_path == "/Users/kumargaurav/Desktop/python-ci/src"
+    #assert index() == "Hello, world!"
