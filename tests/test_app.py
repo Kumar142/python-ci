d6aa93b694b9f595e57ff1df2ebf82dd39bda7eb
gauravk <gauravk@apple.com>
2023-11-01 22:45:25 +0530

    dir change s

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ absolute_path = os.path.abspath(relative_path)
 
 # Append the absolute path to sys.path
 sys.path.append(absolute_path)
-#from app import index
+from app import index
 def test_index():
-    assert absolute_path == "/Users/kumargaurav/Desktop/python-ci/src"
-    #assert index() == "Hello, world!"
+    #assert absolute_path == "/Users/kumargaurav/Desktop/python-ci/src"
+    assert index() == "Hello, world!"
